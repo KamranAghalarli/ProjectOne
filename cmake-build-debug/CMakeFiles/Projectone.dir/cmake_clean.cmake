89a7a0c555871f9a6bf7f38447a864aef58812d8
KamranAghalarli <kamranaghalarli@gmail.com>
2021-03-19 18:32:55 +0400

    Welcome to Project One

---
+++ cmake-build-debug/CMakeFiles/Projectone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projectone.dir/main.c.o"
+  "Projectone"
+  "Projectone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projectone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
